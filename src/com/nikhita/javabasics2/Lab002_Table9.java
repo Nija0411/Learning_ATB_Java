51d32116018dea62c1bb4c0948c3c7e631aeaea4
nija0225 <nikhita.qa@gmail.com>
2025-03-12 12:34:16 +0530

    Basic push

---
+++ src/com/nikhita/javabasics2/Lab002_Table9.java
@@ -0,0 +1,12 @@
+package com.nikhita.javabasics2;
+
+public class Lab002_Table9 {
+    public static void main(String[] args) {
+        System.out.println("Comments Basics");
+        //Single Line comments
+        /* Multiple
+           line
+           comments
+         */
+    }
+}
