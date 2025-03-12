7efa5611dea99d40649997398c27ba484d1abfe0
nija0225 <nikhita.qa@gmail.com>
2025-03-12 23:49:06 +0530

    Updated push

---
+++ src/com/nikhita/interviewque/Int001_InterviewConcatenation.java
@@ -0,0 +1,17 @@
+package com.nikhita.interviewque;
+
+public class Int001_InterviewConcatenation {
+    public static void main(String[] args) {
+        String name = "Nikhita";
+        String lastName = "Jalapure";
+
+        int a = 10;
+        int b = 10;
+
+        System.out.println(name+ lastName + a + b); //NikhitaJalapure1010
+
+        System.out.println(a + b + name + lastName); //20NikhitaJalapure
+
+        System.out.println(name + lastName + (a+b)); //NikhitaJalapure20
+    }
+}
---
+++ src/com/nikhita/tasks/Task001_Table9.java
@@ -0,0 +1,18 @@
+package com.nikhita.tasks;
+
+public class Task001_Table9 {
+    public static void main(String[] args) {
+        System.out.println("Code to print the 9th Table");
+        int table =9;
+        System.out.printf("%d x 1 = %d\n", table, table);
+        System.out.printf("%d x 2 = %d\n", table, table*2);
+        System.out.printf("%d x 3 = %d\n", table, table*3);
+        System.out.printf("%d x 4 = %d\n", table, table*4);
+        System.out.printf("%d x 5 = %d\n", table, table*5);
+        System.out.printf("%d x 6 = %d\n", table, table*6);
+        System.out.printf("%d x 7 = %d\n", table, table*7);
+        System.out.printf("%d x 8 = %d\n", table, table*8);
+        System.out.printf("%d x 9 = %d\n", table, table*9);
+        System.out.printf("%d x 10 = %d", table, table*10);
+    }
+}
